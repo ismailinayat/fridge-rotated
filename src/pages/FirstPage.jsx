a4fd2ca3299daa8bb7947c98e1ea97fbd65f4f08
ismailinayat <ismailinayat@gmail.com>
2021-11-28 10:38:56 +0500

    made video autoplay

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -7,7 +7,7 @@ function FirstPage() {
 
     return (
         <div className='video-page'>
-            <video src='/video3.mp4' id="vid" className="video-home" autoPlay loop controls></video>
+            <video src='/video3.mp4' id="vid" className="video-home" autoPlay loop muted></video>
         </div>
     )
 }
